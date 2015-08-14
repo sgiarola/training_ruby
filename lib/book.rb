fc24d2499da892bfb04f1592b9317553f86d1850
sgiarola <samuel.giarola@gmail.com>
2015-08-13 21:13:44 -0300

    As instance attributes in Ruby are private, getter and setter was created

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,21 @@
 class Book
 
-	def initialize(autor, isbn="1", numero_de_paginas)
+	def initialize(autor, isbn="1", numero_de_paginas, preco)
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
 
 	def to_s
 		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+	end
+
+	def preco
+		@preco
+	end
+
+	def preco=(preco)
+		@preco = preco
 	end
 end
