879b4e9eeaeeedbd492baa76c24beba1b5adeb87
sgiarola <samuel.giarola@gmail.com>
2015-08-14 00:28:04 -0300

    Overwriting methods eql? and hash in Book class

--- lib/book.rb
+++ lib/book.rb
@@ -10,8 +10,17 @@ class Book
 
 	attr_accessor :preco
 	attr_reader :category
+	attr_reader :isbn
 
 	def to_s
 		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}, Preço: #{@preco}, Categoria: #{@category}"
+	end
+
+	def eql?(other_book)
+		@isbn == other_book.isbn
+	end
+
+	def hash
+		@isbn.hash
 	end
 end
