a8eb4d8923e00ed99d45b303675cec0a85afb6f7
sgiarola <samuel.giarola@gmail.com>
2015-08-18 23:32:58 -0300

    Using simplified notation & to map and inject Enumerables.

--- lib/report.rb
+++ lib/report.rb
@@ -5,10 +5,10 @@ class Report
   end
 
   def total
-    @library.books.inject(0) { |tot, book| tot += book.price }
+    @library.books.map(&:price).inject(:+)
   end
 
   def titles
-    @library.books.map { |book| book.title }
+    @library.books.map &:title
   end
 end
