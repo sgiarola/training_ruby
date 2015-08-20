5a708da126a4d52a120de76445f0a303626aee6b
sgiarola <samuel.giarola@gmail.com>
2015-08-19 23:41:49 -0300

    Calculating fine of book using lambda and currying.

--- lib/report.rb
+++ lib/report.rb
@@ -11,4 +11,10 @@ class Report
   def titles
     @library.books.map &:title
   end
+
+  def fine_of_ten_percent_book_value_a_day(days, book)
+    fine_percent = lambda { |percent, book_price| percent * book_price }.curry
+    fine_ten_percent = fine_percent.call 0.1 * days
+    fine_ten_percent.call book.price
+  end
 end
