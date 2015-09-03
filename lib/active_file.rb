f9160df33af482ce390877125175d4a8e9950f24
sgiarola <samuel.giarola@gmail.com>
2015-09-02 21:25:43 -0300

    Simulated Keyword arguments to ActiveFile receive parameters in new Object.

--- lib/active_file.rb
+++ lib/active_file.rb
@@ -52,10 +52,14 @@ module ActiveFile
     base.extend ClassMethods
     base.class_eval do
       attr_reader :id, :destroyed, :new_record
-      def initialize
+      def initialize(parameters = {})
         @id = self.class.next_id
         @destroyed = false
         @new_record = true
+
+        parameters.each do |key, value|
+          instance_variable_set "@#{key}", value
+        end
       end
     end
   end
